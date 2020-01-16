5bf362ba18d5b9c85a1be4ef0481cad3c8220102
unknown <vladikd96@gmail.com>
2020-01-16 20:24:55 +0200

    got started

--- src/components/ChatWindow.js
+++ src/components/ChatWindow.js
@@ -1,10 +1,27 @@
 import React,{Component} from "react";
-export default class ChatWindow extends Component{
-    render(){
+export default function ChatWindowComponent(props){
+     const renderMessage = messages => {
+        return messages.map( message => {
+            if(message.senderId == props.id) {
+                return (
+                    <div className="user messages" key={message.id}>
+                        <p className="message">{message.text}</p>
+                    </div>
+                )
+            } else {
+                return (
+                    <div className="partner messages" key={message.id}>
+                        <p className="message">{message.text}</p>
+                    </div>
+                )
+              }
+            })
+        }
         return (
             <div className="chat-window">
-                <div className="chat-content"></div>
+                <div className="chat-content">
+                    {renderMessage(props.messages)}
+                </div>
             </div>
         )
-    }
 }
--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -1,19 +1,27 @@
 import React,{Component} from "react";
-export default class Rooms extends Component{
-    render(){
+
+export default function Rooms (props){
+
+    const renderRooms=user=>{
+      if(user){
+          return user.rooms.map((room,index)=>{
+              return <li key={index}>{room.name}</li>
+          })
+      }
+    }
         return (
             <div className="rooms">
                 <div className="rooms-container">
                     <h2>Your Rooms:</h2>
                     <ul>
-                        <li># room1</li>
-                        <li># room2</li>
-                        <li># room3</li>
-                        <li># room4</li>
+                        {renderRooms(props.user)}
+                        {/*<li># room1</li>*/}
+                        {/*<li># room2</li>*/}
+                        {/*<li># room3</li>*/}
+                        {/*<li># room4</li>*/}
                     </ul>
                 </div>
                 
             </div>
         )
-    }
 }
